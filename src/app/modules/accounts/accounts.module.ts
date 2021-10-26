38d853cd21667ec0dfcf03bba5bbfe5195968188
S542301 <77767471+S542301@users.noreply.github.com>
2021-10-26 17:38:16 -0500

    Creating shared module and alert Component

--- src/app/modules/accounts/accounts.module.ts
+++ src/app/modules/accounts/accounts.module.ts
@@ -5,13 +5,17 @@ import { AccountsRoutingModule } from './accounts-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
-    AccountsRoutingModule, ReactiveFormsModule
+    AccountsRoutingModule, 
+    ReactiveFormsModule,
+    SharedModule
+
   ]
 })
 export class AccountsModule { }
