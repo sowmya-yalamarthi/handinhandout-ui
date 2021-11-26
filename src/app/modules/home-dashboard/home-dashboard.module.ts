9bc11554e9834ed4f401488f5f706f96ee0abbd1
S542362 <77766701+S542362@users.noreply.github.com>
2021-11-25 19:04:06 -0600

    Creation Of Home DashBoard Module

---
+++ src/app/modules/home-dashboard/home-dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeDashboardRoutingModule } from './home-dashboard-routing.module';
+import { HomedashboardComponent } from './homedashboard/homedashboard.component';
+
+
+@NgModule({
+  declarations: [HomedashboardComponent],
+  imports: [
+    CommonModule,
+    HomeDashboardRoutingModule
+  ]
+})
+export class HomeDashboardModule { }
