08eff1b98c7e46b1bbc19951f5d3385d8dde0796
sowmya-yalamarthi <s542049@nwmissouri.edu>
2021-11-28 18:00:10 -0600

    Books Rent HandOut Sell

---
+++ src/app/modules/books-rent/books-rent.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BooksRentRoutingModule } from './books-rent-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BooksRentRoutingModule
+  ]
+})
+export class BooksRentModule { }
---
+++ src/app/modules/books-sell/books-sell.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BooksSellRoutingModule } from './books-sell-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BooksSellRoutingModule
+  ]
+})
+export class BooksSellModule { }
