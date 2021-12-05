348f5a8132c366201012798dd08bb28035d8ea28
Satyachowdary97 <77590874+Satyachowdary97@users.noreply.github.com>
2021-12-04 21:36:18 -0600

    Add navigation from home dashboard to Books sell module

--- src/app/modules/books-sell/books-sell-routing.module.ts
+++ src/app/modules/books-sell/books-sell-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SellBooksComponent } from './sell-books/sell-books.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: SellBooksComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
